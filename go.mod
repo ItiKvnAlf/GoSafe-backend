590452ff53b9aa955aa64dbbca17a4f6b9547603
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-09-14 18:35:51 -0300

    User Model completed. uuid intalled

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module backend
 
 go 1.21.0
+
+require github.com/google/uuid v1.3.1 // indirect
--- models/userModel.go
+++ models/userModel.go
@@ -1,1 +1,15 @@
 package models
+
+import "github.com/google/uuid"
+
+// User struct
+type User struct {
+	ID         uuid.UUID `gorm:"not null;unique_index" json:"id"`
+	Name       string    `gorm:"not null;unique_index" json:"name"`
+	Email      string    `gorm:"not null;unique_index" json:"email"`
+	Password   string    `gorm:"not null" json:"password"`
+	Phone      string    `gorm:"not null;unique_index" json:"phone"`
+	Address    string    `gorm:"not null" json:"address"`
+	ProfilePic string    `gorm:"not null" json:"profile_pic"`
+	Rut        string    `gorm:"not null;unique_index" json:"rut"`
+}
