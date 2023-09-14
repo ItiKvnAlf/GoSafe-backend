3c01a72ed7277f7278872ca7a66f46e3224a0fb1
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-09-14 18:44:01 -0300

    orm installed and use in userModel

--- models/userModel.go
+++ models/userModel.go
@@ -1,9 +1,13 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 // User struct
 type User struct {
+	gorm.Model
 	ID         uuid.UUID `gorm:"not null;unique_index" json:"id"`
 	Name       string    `gorm:"not null;unique_index" json:"name"`
 	Email      string    `gorm:"not null;unique_index" json:"email"`
