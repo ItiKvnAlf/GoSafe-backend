71f95712167889b8c31492f9fb6a1fec25a83f71
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-09-14 19:14:42 -0300

    Contact model modified

--- models/contactModel.go
+++ models/contactModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// Contact struct
+type Contact struct {
+	gorm.Model
+
+	ID    uuid.UUID `gorm:"not null;unique_index" json:"id"`
+	Name  string    `gorm:"not null;unique_index" json:"name"`
+	Email string    `gorm:"not null;unique_index" json:"email"`
+	Phone string    `gorm:"not null;unique_index" json:"phone"`
+}
