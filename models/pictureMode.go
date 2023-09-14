66ad48ced72cfe86b4afdfd1128313d9b47791a0
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-09-14 18:54:16 -0300

    PictureModel complete with ormt

--- models/pictureMode.go
+++ models/pictureMode.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// PictureMode struct
+type PictureMode struct {
+	gorm.Model
+
+	ID     uuid.UUID `gorm:"not null;unique_index" json:"id"`
+	Image  string    `gorm:"not null;unique_index" json:"image"`
+	UserID string    `gorm:"not null;unique_index" json:"user_id"`
+	User   User      `gorm:"foreignKey:UserID"`
+}
