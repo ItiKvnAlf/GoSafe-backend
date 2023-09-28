9a5d4b32466a1af82b6143c24736ef98b37b1369
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-09-28 18:35:42 -0300

    Feat: GetContacts

--- controllers/contactController.go
+++ controllers/contactController.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	db "backend/config"
+	"backend/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetContacts(c *fiber.Ctx) error {
+	var contacts []models.Contact
+	db.DB.Select(("id, user_id, name, email, phone")).Find(&contacts)
+
+	return c.Status(200).JSON(fiber.Map{
+		"success": true,
+		"message": "success",
+		"data":    contacts,
+	})
+
+}
