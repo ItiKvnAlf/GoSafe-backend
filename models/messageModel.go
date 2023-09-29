4ccac3eb42d73b06fe03ab47d559e9de68e3c764
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-09-29 10:07:05 -0300

    Fix: Message Model

--- models/messageModel.go
+++ models/messageModel.go
@@ -10,10 +10,11 @@ type Message struct {
 	UserID         uuid.UUID `gorm:"not null" json:"user_id"`
 	TravelRouteID  uuid.UUID `gorm:"not null" json:"travel_route_id"`
 	GeolocationID  uuid.UUID `gorm:"not null" json:"geolocation_id"`
-	DefaultMessage string    `gorm:"not null" json:"default_message"`
 	LastPicture    uuid.UUID `gorm:"not null" json:"last_picture"`
+	DefaultMessage string    `gorm:"not null" json:"default_message"`
 
 	User        User        `gorm:"foreignkey:UserID"`
 	TravelRoute TravelRoute `gorm:"foreignkey:TravelRouteID"`
 	Geolocation Geolocation `gorm:"foreignkey:GeolocationID"`
+	Picture     Picture     `gorm:"foreignkey:LastPicture"`
 }
