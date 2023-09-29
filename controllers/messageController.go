3b8a9d8b13a8b7698115b37ea8198d64d5b1b352
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-09-29 10:07:49 -0300

    Feat: Get Message by Travel

--- controllers/messageController.go
+++ controllers/messageController.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	db "backend/config"
+	"backend/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetMessageTravel(c *fiber.Ctx) error {
+
+	travelRouteID := c.Params("travel_route_id")
+
+	var message []models.Message
+	db.DB.Select(("id,user_id,travel_route_id,geolocation_id,last_picture,default_message")).Where("travel_route_id = ?", travelRouteID).Find(&message)
+
+	return c.Status(200).JSON(fiber.Map{
+		"success": true,
+		"message": "success",
+		"data":    message,
+	})
+}
