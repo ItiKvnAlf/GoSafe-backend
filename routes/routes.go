6a6baeb45cb0aa041b6a20227961026b4390b9b1
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-10-06 10:48:34 -0300

    Feat: add routes

--- routes/routes.go
+++ routes/routes.go
@@ -15,4 +15,20 @@ func Setup(app *fiber.App) {
 	userGroup.Get("/:id", controllers.GetUser)
 	userGroup.Put("/:id", controllers.UpdateUser)
 
+	contactGroup := app.Group("/contacts")
+
+	contactGroup.Get("/", controllers.GetContacts)
+	contactGroup.Get("/:user_id", controllers.GetContactUser)
+	contactGroup.Post("/", controllers.CreateContact)
+
+	messageGroup := app.Group("/messages")
+
+	messageGroup.Get("/:travel_route_id", controllers.GetMessageTravel)
+	messageGroup.Post("/", controllers.CreateMesssage)
+
+	pictureGroup := app.Group("/pictures")
+
+	pictureGroup.Get("/:travel_route_id", controllers.GetPicturesTravel)
+	pictureGroup.Post("/", controllers.CreatePicture)
+
 }
