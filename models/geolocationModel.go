414bba79701b6d1b7529500f6eec189e4197d812
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2023-12-13 15:11:39 -0300

    Update CurrentPoint data type in Geolocation struct

--- models/geolocationModel.go
+++ models/geolocationModel.go
@@ -7,6 +7,6 @@ import (
 // Geolocation struct
 type Geolocation struct {
 	ID            uuid.UUID `gorm:"primaryKey;" json:"id"`
-	CurrentPoint  float64   `gorm:"not null" json:"current_point"`
+	CurrentPoint  string    `gorm:"not null" json:"current_point"`
 	TravelRouteID uuid.UUID `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL,foreignkey:TravelRouteID" json:"travel_route_id"`
 }
